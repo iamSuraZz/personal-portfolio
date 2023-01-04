fe1db198972fcc77b5cfbaef79c653ae4f2f2147
SuraZz007 <surajdev9886@gmail.com>
2023-01-04 21:31:28 +0530

    make contact form

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Banner from "./components/Banner";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
