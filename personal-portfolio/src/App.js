b4467ba3ec78675e41fac4aec210bd22ef8fef65
SuraZz007 <surajdev9886@gmail.com>
2023-01-04 12:09:27 +0530

    create navigation bar

---
+++ personal-portfolio/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
