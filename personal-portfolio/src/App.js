34dc228d5ba176e2311a1c45150f8a9f85e2aef0
SuraZz007 <surajdev9886@gmail.com>
2023-01-04 14:25:31 +0530

    make banner for portfolio website

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }
---
+++ personal-portfolio/src/components/Banner.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
+const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = [
+    "Web Developer.",
+    "React / React Native Developer.",
+    "Backend (MERN) Developer.",
+    "UI/UX Designer.",
+  ];
+
+  const [text, setText] = useState("");
+
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevData) => prevData / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aling-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>
+              {`Hi I'm Suraj, `}
+              <span className="wrap">{text}</span>
+            </h1>
+            <p>Some lorem ipsum text for now</p>
+            <button onClick={() => console.log("clicked")}>
+              Let’s connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Banner;
