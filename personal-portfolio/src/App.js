9ec2dd181126c4e4976d0c192cfcb4dc0b223507
SuraZz007 <surajdev9886@gmail.com>
2023-01-04 14:51:20 +0530

    make skills section

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Banner from "./components/Banner";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
